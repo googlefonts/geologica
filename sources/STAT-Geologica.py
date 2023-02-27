9581286a9c365861e293b3317abd9a5cd37c3677
Viviana Monsalve <viviana.monsalve.a@gmail.com>
2023-02-27 10:55:47 -0500

    STAT file only for full VF family

--- sources/STAT-Geologica.py
+++ sources/STAT-Geologica.py
@@ -59,81 +59,7 @@ LOCATIONS = [
 ]
 
 
-UPRIGHT_AXES = [
-    dict(
-        tag="SHRP",
-        name="Sharpness",
-        ordering=0,
-        values=[
-            dict(value=0, name="Blunt", flags=0x2),
-            dict(value=100, name="Sharp"),
-        ],
-    ),
-    dict(
-        tag="wght",
-        name="Weight",
-        ordering=1,
-        values=[
-            dict(value=100, name="Thin"),
-            dict(value=200, name="ExtraLight"),
-            dict(value=300, name="Light"),
-            dict(value=400, name="Regular", flags=0x2, linkedValue=700),
-            dict(value=500, name="Medium"),
-            dict(value=600, name="SemiBold"),
-            dict(value=700, name="Bold"),
-            dict(value=800, name="ExtraBold"),
-            dict(value=900, name="Black"),
-        ],
-    ),
-    dict(
-        tag="ital",
-        name="Italic",
-        ordering=2,
-        values=[
-            dict(value=0, name="Roman", flags=0x2, linkedValue=1), #Upright
-        ],
-    ),
-]
-
-ITALIC_AXES = [
-    dict(
-        tag="SHRP",
-        name="Sharpness",
-        ordering=0,
-        values=[
-            dict(value=0, name="Blunt", flags=0x2),
-            dict(value=100, name="Sharp"),
-        ],
-    ),
-    dict(
-        tag="wght",
-        name="Weight",
-        ordering=1,
-        values=[
-            dict(value=100, name="Thin"),
-            dict(value=200, name="ExtraLight"),
-            dict(value=300, name="Light"),
-            dict(value=400, name="Regular", flags=0x2, linkedValue=700),
-            dict(value=500, name="Medium"),
-            dict(value=600, name="SemiBold"),
-            dict(value=700, name="Bold"),
-            dict(value=800, name="ExtraBold"),
-            dict(value=900, name="Black"),
-        ],
-    ),
-    dict(
-        tag="ital",
-        name="Italic",
-        ordering=2,
-        values=[
-            dict(value=1, name="Italic"), #Italic
-        ],
-    ),
-]
-
-fullVF = "../fonts/variable/full/Geologica[CRSV,SHRP,slnt,wght].ttf"
-splitRomaVF = "../fonts/variable/split/Geologica[SHRP,wght].ttf"
-splitItalicVF = "../fonts/variable/split/Geologica-Italic[SHRP,wght].ttf"
+fullVF = "../fonts/variable/Geologica[CRSV,SHRP,slnt,wght].ttf"
 
 
 def main():
@@ -143,21 +69,6 @@ def main():
     buildStatTable(tt, AXES, LOCATIONS)
     tt.save(filepath)
     print(f"[STAT TABLE] Added STAT table to {filepath}")
-
-    # process Upright VF
-    filepath = splitRomaVF
-    tt = TTFont(filepath)
-    buildStatTable(tt, UPRIGHT_AXES)
-    tt.save(filepath)
-    print(f"[STAT TABLE] Added STAT table to {filepath}")
-
-    # process Italic VF
-    filepath = splitItalicVF
-    tt = TTFont(filepath)
-    buildStatTable(tt, ITALIC_AXES)
-    tt.save(filepath)
-    print(f"[STAT TABLE] Added STAT table to {filepath}")
-
 
 if __name__ == "__main__":
     main()
