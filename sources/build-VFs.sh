f67b60d341bd57fca4e6a5cc58bcb5fc4d4a41d9
Viviana Monsalve <viviana.monsalve.a@gmail.com>
2023-02-27 10:43:05 -0500

    build-VFs updated to build full + split vfs

--- sources/build-VFs.sh
+++ sources/build-VFs.sh
@@ -1,34 +1,63 @@
-### Run in the terminal by entering this file path (must be given execute permissions with chmod)
-### requires a python 3 environment
+# source venv/bin/activate
 
-#!/bin/sh
-#source ../venv/bin/activate
 set -e
 
-# echo "Creating single VFs dir"
-# mkdir -p ../fonts/variable/singleVFs/
+outputDir="../fonts/variable/full"
+splitDir="../fonts/variable/split"
+vfFullName="Geologica[CRSV,SHRP,slnt,wght].ttf"
+vfRomanName="Geologica[SHRP,wght].ttf"
+vfItalicName="Geologica-Italic[SHRP,wght].ttf"
 
-vf=Geologica[CRSV,SHRP,slnt,wght].ttf
+mkdir -p "$outputDir" "$splitDir"
 
+vfFullPath="$outputDir/$vfFullName"
+vfRomanPath="$splitDir/$vfRomanName"
+vfItalicPath="$splitDir/$vfItalicName"
 
-###################################################
-######### generate Roman+Ital variable font ###########
+######### generate variable font ###########
 
+echo "----GENERATING FULL VF----"
 
-echo "----Build Roman-Ital VF----"
+fontmake -g Geologica.glyphs -f --no-check-compatibility -o variable --output-path "$vfFullPath"
 
-gftools builder config.yaml
+########## generating split VFs ############
 
-
-###################################################
-######### generate single variable font ###########
-
-
-echo "----Building Roman VF----"
-
-fonttools varLib.instancer ../fonts/variable/Roman-Ital/$vf CRSV=0 slnt=0 --output ../fonts/variable/Geologica[SHRP,wght].ttf
-
+echo "----GENERATING SPLIT VFS----"
 
 echo "----Building Roman VF----"
+fonttools varLib.instancer $vfFullPath CRSV=0 slnt=0 --output $vfRomanPath
 
-fonttools varLib.instancer ../fonts/variable/Roman-Ital/$vf CRSV=1 slnt=-12 --output ../fonts/variable/Geologica-Italic[SHRP,wght].ttf
+echo "----Building Italic VF----"
+fonttools varLib.instancer $vfFullPath CRSV=1 slnt=-12 --output $vfItalicPath
+
+########## postprocessing ############
+
+echo "----Postprocessing----"
+
+
+# fix nonhinting full VF
+echo "Full VF"
+gftools fix-nonhinting "$vfFullPath" "$vfFullPath"
+rm "${vfFullPath/.ttf/-backup-fonttools-prep-gasp.ttf}"
+
+# fix nonhinting Roman VF
+echo "Roman VF"
+gftools fix-nonhinting "$vfRomanPath" "$vfRomanPath"
+rm "${vfRomanPath/.ttf/-backup-fonttools-prep-gasp.ttf}"
+
+# fix nonhinting Italic VF
+echo "Italic VF"
+gftools fix-nonhinting "$vfItalicPath" "$vfItalicPath"
+rm "${vfItalicPath/.ttf/-backup-fonttools-prep-gasp.ttf}"
+
+# add STAT table
+python3 STAT-Geologica.py $vfFullPath
+
+
+############### Fontbakery ##################
+
+# fontbakery check-googlefonts -l WARN "$vfFullPath" --ghmarkdown ../fonts/checks/checks-variable-full.md 
+
+# fontbakery check-googlefonts -l WARN "$vfRomanPath" --ghmarkdown ../fonts/checks/checks-variable-Roman.md
+
+fontbakery check-googlefonts -l WARN "$vfItalicPath" --ghmarkdown ../fonts/checks/checks-variable-Italicmd
